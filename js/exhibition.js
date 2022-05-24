c033af6aa7107af5586e63d630ee7d4850355d71
boramiiii <b_ram1227@naver.com>
2022-05-24 15:39:36 +0900

    gnb change

--- js/exhibition.js
+++ js/exhibition.js
@@ -1,3 +1,230 @@
+
+const rolling = exhi.querySelector(".rolling"); 
+const prev = document.querySelector(".prev"); 
+const next = document.querySelector(".next"); 
+let num = -320; 
+let wid = 0; 
+let timer = null; 
+let enableClick = true; 
+let picSrc = null;
+
+
+createList("data.json");
+
+
+
+timer = setInterval(move, 50);
+
+rolling.addEventListener("mouseenter", ()=>{
+    clearInterval(timer); 
+});
+
+rolling.addEventListener("mouseleave", ()=>{
+    timer = setInterval(move, 50);
+}); 
+
+next.addEventListener("click", ()=>{
+    if(enableClick){
+        nextEl();     
+        enableClick = false; 
+    }
+}); 
+
+next.addEventListener("mouseenter",()=>{
+    clearInterval(timer); 
+})
+next.addEventListener("mouseleave",()=>{
+    timer = setInterval(move, 50);
+})
+
+
+prev.addEventListener("click", ()=>{
+    if(enableClick){
+        prevEl();
+        enableClick = false; 
+    }
+})
+prev.addEventListener("mouseenter",()=>{
+    clearInterval(timer); 
+})
+prev.addEventListener("mouseleave",()=>{
+    timer = setInterval(move, 50);
+})
+
+
+function createList(url){
+    fetch(url)
+    .then(data=>{ 
+        return data.json();   
+    })
+    .then(json=>{   
+    
+        let items = json.imgSrc; 
+        let tags =''; 
+    
+        items.forEach(item=>{
+            tags+=`
+                <li class="swiper-slide">
+                        <div class="pic" cursor-class="arrow">
+                            <img src=${item.pic} alt="서울전시-히토슈라이얼">
+                        </div>
+                        <div class="con">
+                            <h2>
+                                <span>${item.loca}</span>
+                                ${item.name}
+                            </h2>
+                            <p>
+                                ${item.date}
+                            </p>
+                        </div>
+                </li>
+            `;
+            
+        }); 
+    
+        rolling.innerHTML = tags; 
+    
+        initList(); 
+    });
+}
+
+let content = document.querySelector(".contentWrap");
+let articleWrap = content.querySelector(".articleWrap");
+let btnclose = content.querySelector(".close");
+
+function createContent(url, imgsrc){
+    fetch(url)
+    .then(data=>{ 
+        return data.json();   
+    })
+    .then(json=>{   
+        let items = json.imgSrc; 
+        let tags =''; 
+    
+        items.forEach(item=>{
+            
+            if(item.pic === imgsrc){
+                tags = `
+                    
+                    <article>
+                        <img src=${imgsrc}>
+                    </article>
+                    <article>
+                        <h3>${item.name}</h3>
+                        <p>
+                            ${item.txt}
+                        </p>
+                    </article>
+
+                    `;
+                    articleWrap.innerHTML = tags; 
+            }
+        }); 
+    });
+
+    
+    new Anime(articleWrap,{
+        prop:"opacity",
+        value:1,
+        duration:500
+    });
+}
+
+
+rolling.addEventListener("click",(e)=>{
+    e.preventDefault();
+
+    if(!e.target.closest(".pic img")){
+        return;
+    }
+
+    const imgsrc = e.target.getAttribute("src");
+    createContent("data.json", imgsrc);
+
+    new Anime(btnclose, {
+        prop:"opacity",
+        value:1,
+        duration:500,
+    });
+})
+
+btnclose.addEventListener("click",()=>{
+    new Anime(articleWrap,{
+        prop:"opacity",
+        value:0,
+        duration:500,
+        callback:()=>{
+            articleWrap.innerHTML = '';
+        }
+    })
+
+    new Anime(btnclose, {
+        prop:"opacity",
+        value:0,
+        duration:500,
+    });
+    
+})
+
+
+
+
+function initList(){
+    const article = rolling.querySelectorAll("li"); 
+    const len = article.length; 
+    wid = parseInt(getComputedStyle(article[0]).width);
+    wid = wid + 20;
+    rolling.style.width = len * wid +"px"; 
+    rolling.style.marginRight = -wid +"px";
+    rolling.prepend(rolling.lastElementChild); 
+}
+
+
+
+
+
+function nextEl(){
+    
+    new Anime(rolling,{
+        prop:"margin-right", 
+        value : -wid * 2, 
+        duration: 500, 
+        callback:()=>{
+            rolling.prepend(rolling.lastElementChild); 
+            rolling.style.marginRight = -wid +"px"; 
+            enableClick = true; 
+        }
+    })
+}
+
+function prevEl(){
+    
+    new Anime(rolling, {
+        prop:"margin-right", 
+        value : 0, 
+        duration: 500, 
+        callback:()=>{
+            rolling.append(rolling.firstElementChild); 
+            rolling.style.marginRight = -wid +"px"; 
+            enableClick = true; 
+        }
+    })
+}
+
+function move(){
+    if(num < -wid *2){
+        num = -wid; 
+        rolling.prepend(rolling.lastElementChild); 
+    }else{
+        num-=2; 
+    }    
+    rolling.style.marginRight = num +"px"; 
+}
+
+
+
+
+/////////////////////////////////////////////////////////////
 // const cursor = document.querySelector('#cursor');
 // const cursorCircle = cursor.querySelector('.cursor__circle');
 // const inner = document.querySelector(".inner");
@@ -80,223 +307,3 @@
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////
-
-
-const rolling = exhi.querySelector(".rolling"); 
-const prev = document.querySelector(".prev"); 
-const next = document.querySelector(".next"); 
-let num = -320; 
-let wid = 0; 
-let timer = null; 
-let enableClick = true; 
-let picSrc = null;
-
-
-createList("data.json");
-
-
-
-timer = setInterval(move, 50);
-
-rolling.addEventListener("mouseenter", ()=>{
-    clearInterval(timer); 
-});
-
-rolling.addEventListener("mouseleave", ()=>{
-    timer = setInterval(move, 50);
-}); 
-
-next.addEventListener("click", ()=>{
-    if(enableClick){
-        nextEl();     
-        enableClick = false; 
-    }
-}); 
-
-next.addEventListener("mouseenter",()=>{
-    clearInterval(timer); 
-})
-next.addEventListener("mouseleave",()=>{
-    timer = setInterval(move, 50);
-})
-
-
-prev.addEventListener("click", ()=>{
-    if(enableClick){
-        prevEl();
-        enableClick = false; 
-    }
-})
-prev.addEventListener("mouseenter",()=>{
-    clearInterval(timer); 
-})
-prev.addEventListener("mouseleave",()=>{
-    timer = setInterval(move, 50);
-})
-
-
-function createList(url){
-    fetch(url)
-    .then(data=>{ 
-        return data.json();   
-    })
-    .then(json=>{   
-    
-        let items = json.imgSrc; 
-        let tags =''; 
-    
-        items.forEach(item=>{
-            tags+=`
-                <li class="swiper-slide">
-                        <div class="pic"">
-                            <img src=${item.pic} alt="서울전시-히토슈라이얼">
-                        </div>
-                        <div class="con">
-                            <h2>
-                                <span>${item.loca}</span>
-                                ${item.name}
-                            </h2>
-                            <p>
-                                ${item.date}
-                            </p>
-                        </div>
-                </li>
-            `;
-            
-        }); 
-    
-        rolling.innerHTML = tags; 
-    
-        initList(); 
-    });
-}
-
-let content = document.querySelector(".contentWrap");
-let articleWrap = content.querySelector(".articleWrap");
-let btnclose = content.querySelector(".close");
-
-function createContent(url, imgsrc){
-    fetch(url)
-    .then(data=>{ 
-        return data.json();   
-    })
-    .then(json=>{   
-    
-        let items = json.imgSrc; 
-        let tags =''; 
-    
-        items.forEach(item=>{
-            
-            if(item.pic === imgsrc){
-                tags = `
-                    
-                    <article>
-                        <img src=${imgsrc}>
-                    </article>
-                    <article>
-                        <h3>${item.name}</h3>
-                        <p>
-                            ${item.txt}
-                        </p>
-                    </article>
-
-                    `;
-                    articleWrap.innerHTML = tags; 
-            }
-            
-        }); 
-    
-    });
-}
-
-
-rolling.addEventListener("click",(e)=>{
-    e.preventDefault();
-
-    const imgsrc = e.target.getAttribute("src");
-    createContent("data.json", imgsrc);
-
-    new Anime(btnclose, {
-        prop:"opacity",
-        value:1,
-        duration:500,
-    });
-})
-
-btnclose.addEventListener("click",()=>{
-    articleWrap.innerHTML = '';
-
-    new Anime(btnclose, {
-        prop:"opacity",
-        value:0,
-        duration:500,
-    });
-})
-
-
-
-function showCloseBtn(){
-    new Anime(btnclose, {
-        prop:"display",
-        value:"block",
-        duration:500,
-    });
-
-    new Anime(btnclose, {
-        prop:"opacity",
-        value:1,
-        duration:500,
-    });
-}
-
-function initList(){
-    const article = rolling.querySelectorAll("li"); 
-    const len = article.length; 
-    wid = parseInt(getComputedStyle(article[0]).width);
-    wid = wid + 20;
-    rolling.style.width = len * wid +"px"; 
-    rolling.style.marginRight = -wid +"px";
-    rolling.prepend(rolling.lastElementChild); 
-}
-
-
-
-
-
-function nextEl(){
-    
-    new Anime(rolling,{
-        prop:"margin-right", 
-        value : -wid * 2, 
-        duration: 500, 
-        callback:()=>{
-            rolling.prepend(rolling.lastElementChild); 
-            rolling.style.marginRight = -wid +"px"; 
-            enableClick = true; 
-        }
-    })
-}
-
-function prevEl(){
-    
-    new Anime(rolling, {
-        prop:"margin-right", 
-        value : 0, 
-        duration: 500, 
-        callback:()=>{
-            rolling.append(rolling.firstElementChild); 
-            rolling.style.marginRight = -wid +"px"; 
-            enableClick = true; 
-        }
-    })
-}
-
-function move(){
-    if(num < -wid *2){
-        num = -wid; 
-        rolling.prepend(rolling.lastElementChild); 
-    }else{
-        num-=2; 
-    }    
-    rolling.style.marginRight = num +"px"; 
-}
